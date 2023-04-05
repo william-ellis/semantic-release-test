18e3370c67143ff41bc84d2726f796399732ef9d
william-ellis <11599796+william-ellis@users.noreply.github.com>
2023-04-05 18:12:04 +0200

    Publish via nuget

--- Publish.ps1
+++ Publish.ps1
@@ -7,5 +7,28 @@ param(
     [System.Version]$Version
 )
 
+function New-TemporaryDirectory {
+    $parent = [System.IO.Path]::GetTempPath()
+    $name = [System.IO.Path]::GetRandomFileName()
+    New-Item -ItemType Directory -Path (Join-Path $parent $name)
+}
+
 Update-ModuleManifest $ManifestPath -ModuleVersion $Version
-Publish-PSResource -Path $ManifestPath -Repository LocalRepo -SkipDependenciesCheck -SkipModuleManifestValidate
+
+try {
+    $repoDir = New-TemporaryDirectory
+    $repoName = [string](New-Guid)
+    Register-PSResourceRepository -Name $repoName -Uri $repoDir
+
+    Publish-PSResource -Path $ManifestPath -Repository $repoName -SkipDependenciesCheck -SkipModuleManifestValidate
+    
+    $nupkg = Get-ChildItem $repoDir -Filter *.nupkg
+    Write-Verbose "Publishing $nupkg"
+    dotnet nuget push $nupkg `
+        --source https://pkgs.dev.azure.com/william-ellis/_packaging/william-ellis/nuget/v3/index.json `
+        --api-key notused
+}
+finally {
+    Unregister-PSResourceRepository -Name $repoName
+    Remove-Item $repoDir -Force -Recurse -ErrorAction Ignore
+}
