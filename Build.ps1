53bb1e2a020a7a8b221c882e3b3641fc218d596d
william-ellis <11599796+william-ellis@users.noreply.github.com>
2023-04-05 17:31:38 +0200

    fix: Remove parameter

--- Build.ps1
+++ Build.ps1
@@ -1,16 +1,8 @@
 [CmdletBinding()]
-param(
-    [AllowEmptyString()]
-    [Parameter(Mandatory)]
-    [string]$Version
-)
+param()
 
 Remove-Item .\output -Force -Recurse -ErrorAction Ignore
 New-Item .\output -ItemType Directory | Out-Null
-
-if ($Version -ne '') {
-    Update-ModuleManifest Test.psd1 -ModuleVersion $Version
-}
 
 Copy-Item Test.psd1 output
 Copy-Item Test.psm1 output
